391396d5b48459c6f1a51810a258e87e98699034
Jayant Seth <64739983+JayantSeth@users.noreply.github.com>
2023-05-15 21:09:29 +0530

    Update and rename setup.py to __init__.py

--- pyaudio/__init__.py
+++ pyaudio/__init__.py
@@ -6,8 +6,8 @@ from os.path import exists, join
 class PyAudio(PythonRecipe):
 
     site_packages_name = 'Pyaudio'
-    version = '0.2.11'
-    url = 'http://example.com/example-{version}.tar.gz'
+    version = '0.2.13'
+    url = 'https://files.pythonhosted.org/packages/91/a0/f439da954d78a987298cb8d1ca1b141c53b1d1d1c7a50e17198ed061b9ac/PyAudio-0.2.13.tar.gz'
 
 
     depends = ['python3', 'portaudio']  
