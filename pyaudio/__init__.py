79e214a937a8976b80ab992be4cf034a78db73be
Jayant Seth <64739983+JayantSeth@users.noreply.github.com>
2023-05-15 22:25:19 +0530

    Update __init__.py

--- pyaudio/__init__.py
+++ pyaudio/__init__.py
@@ -10,7 +10,7 @@ class PyAudio(PythonRecipe):
     url = 'https://files.pythonhosted.org/packages/91/a0/f439da954d78a987298cb8d1ca1b141c53b1d1d1c7a50e17198ed061b9ac/PyAudio-0.2.13.tar.gz'
 
 
-    depends = ['python3', 'portaudio', 'portaudio19-dev', 'python-pyaudio']  
+    depends = ['python3', 'portaudio', 'portaudio19-dev', 'python-pyaudio', 'gcc-multilib']  
 
 
 
